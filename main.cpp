a30df00169d1e8037a7d994f211a7defb18a2ac4
Matthias Bidlingmeyer <m.bidlingmeyer@laserhub.com>
2023-06-21 08:20:52 +0200

    initialize pigpio lib

--- main.cpp
+++ main.cpp
@@ -26,42 +26,40 @@ std::vector<std::string> split(std::string str, std::string token){
 
 
 int main(int argc, char** argv) {
+  std::vector<string> args(argv + 1, argv + argc);
+  string commands;
+  int pin;
+  int delay;
+  string s, tmp;
+  vector<string> commandsVector;
+  pin = 6; // Used pin (WiringPiSchema)
+  delay= 100; // in milliseconds
+  for (auto i = args.begin(); i != args.end(); ++i) {
+      if (*i == "-h" || *i == "--help") {
+          cout << "Syntax: " << argv[0] << " -p <pin [6]> -c <command(s) [0x20(,0x21)]>" << endl;
+          return 0;
+      } else if (*i == "-c") {
+    commands = *++i;
+    commandsVector = split(commands, ",");
+      } else if (*i == "-p") {
+          pin =  std::stoi(*++i, nullptr, 0);
+      }
+  }
 
+  cout << "GPIO Pin is " << pin << endl;
 
-    std::vector<string> args(argv + 1, argv + argc);
-    string commands;
-    int pin;
-    int delay;
-    string s, tmp;
-    vector<string> commandsVector;
-    pin = 6; // Used pin (WiringPiSchema)
-    delay= 100; // in milliseconds
-    for (auto i = args.begin(); i != args.end(); ++i) {
-        if (*i == "-h" || *i == "--help") {
-            cout << "Syntax: " << argv[0] << " -p <pin [6]> -c <command(s) [0x20(,0x21)]>" << endl;
-            return 0;
-        } else if (*i == "-c") {
-	    commands = *++i;
-	    commandsVector = split(commands, ",");
-        } else if (*i == "-p") {
-            pin =  std::stoi(*++i, nullptr, 0);
-        }
+  if (gpioInitialise() >= 0)
+  {
+    OnkyoRI ori(pin);
+
+    for(auto const& commandString: commandsVector) {
+      int commandInteger = std::stoi(commandString, nullptr, 0);
+      cout << "Sending command: " << commandInteger << endl;
+      ori.send(commandInteger);
+      std::this_thread::sleep_for(std::chrono::milliseconds(delay));
     }
+  }
 
-
-
-
- cout << "GPIO Pin is " << pin << endl;
-
- wiringPiSetup();
- OnkyoRI ori(pin);
-
-   for(auto const& commandString: commandsVector) {
-	int commandInteger = std::stoi(commandString, nullptr, 0);
-	cout << "Sending command: " << commandInteger << endl;
-	ori.send(commandInteger);
-	std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-   }
-
+  gpioTerminate();
 }
 
